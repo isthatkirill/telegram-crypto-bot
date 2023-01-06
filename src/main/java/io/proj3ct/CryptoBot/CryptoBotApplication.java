17fa68252264ef80c8692a49235e9cef03451d54
isthatkirill <kirill.emelyanoff2021@yandex.ru>
2023-01-07 00:23:18 +0300

    feat: init bot

---
+++ src/main/java/io/proj3ct/CryptoBot/CryptoBotApplication.java
@@ -0,0 +1,13 @@
+package io.proj3ct.CryptoBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoBotApplication.class, args);
+	}
+
+}
