4a9185f8265c1c909892332bd00ffa2aff1a2e0c
isthatkirill <kirill.emelyanoff2021@yandex.ru>
2023-01-09 02:18:40 +0300

    feat: /top100 command added

---
+++ src/main/java/isthatkirill/CryptoBot/service/Parser.java
@@ -0,0 +1,40 @@
+package isthatkirill.CryptoBot.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.*;
+
+@Slf4j
+public class Parser {
+
+    public Parser() {
+    }
+
+    public String mostPopularCrypto () {
+
+        String textToSend = "TOP-100 cryptocurrencies statistics: \n\n";
+
+        try {
+            Document document = Jsoup.connect("https://www.coingecko.com/").get();
+
+            ArrayList<String> name = new ArrayList<>(Arrays.asList((document.getElementsByAttributeValue("class",
+                    "d-lg-inline " + "font-normal text-3xs tw-ml-0 md:tw-ml-2 md:tw-self-center tw-text-gray-500 " +
+                            "dark:tw-text-white dark:tw-text-opacity-60").text()).split(" ")));
+
+            ArrayList<String> price = new ArrayList<String>(Arrays.asList((document.getElementsByAttribute("data" +
+                    "-coin-symbol").text()).split("  ")));
+
+            for (int i = 0; i < name.size(); i++) {
+                textToSend = textToSend + name.get(i) + ": " + price.get(i) + "\n";
+            }
+
+        } catch (Exception e) {
+            log.error("Error while parsing: " + e.getMessage());
+        }
+        return textToSend;
+    }
+
+}
--- src/main/java/isthatkirill/CryptoBot/service/TelegramBot.java
+++ src/main/java/isthatkirill/CryptoBot/service/TelegramBot.java
@@ -12,6 +12,7 @@ import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScope
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,12 +22,19 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     final BotConfig config;
     static final String HELP_TEXT = "This bot is created by \n @isthatkirill.";
+    Parser parser;
+
 
     public TelegramBot (BotConfig config) {
+
+        parser = new Parser();
         this.config = config;
         List<BotCommand> listOfCommands = new ArrayList<>();
-        listOfCommands.add(new BotCommand("/start", "get a welcome message"));
-        listOfCommands.add(new BotCommand("/data", "get your data"));
+        parser.mostPopularCrypto();
+
+
+        listOfCommands.add(new BotCommand("/start", "Get a welcome message"));
+        listOfCommands.add(new BotCommand("/top100", "Show statistics on popular crypto"));
         listOfCommands.add(new BotCommand("/deletedata", "delete your data"));
         listOfCommands.add(new BotCommand("/help", "about commands"));
         listOfCommands.add(new BotCommand("/settings", "set your preferences"));
@@ -68,13 +76,27 @@ public class TelegramBot extends TelegramLongPollingBot {
                     log.info("[/help] Replied to user " + update.getMessage().getChat().getFirstName());
                     break;
 
+                case "/top100":
+                    sendMessage(chatId, "Enter quantity of cryptocurrencies: ");
+                    //onUpdateReceivedEmbedded()
+                    sendMessage(chatId, parser.mostPopularCrypto());
+                    log.info("[/top100] Replied to user " + update.getMessage().getChat().getFirstName());
+                    break;
+
                 default:
                     sendMessage(chatId, "Sorry, there is no such command! ");
                     log.info("[no command] Replied to user " + update.getMessage().getChat().getFirstName());
 
             }
         }
+    }
 
+    public String onUpdateReceivedEmbedded(Update update) {
+        String embeddedText = "";
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            embeddedText = update.getMessage().getText();
+        }
+        return embeddedText;
     }
 
     private void startCommandReceived(long chatId, String name)  {
